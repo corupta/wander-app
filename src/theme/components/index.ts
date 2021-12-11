18e990649cf9d7cdbd3a782e9debe0fefbc7e33c
okaygenc <okygnc@gmail.com>
2021-12-11 18:02:33 +0300

    added HPLoader component

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,3 +1,5 @@
 export { default as HPText } from './HPText'
 export { default as HPPressable } from './HPPressable'
 export { default as HPButton } from './HPButton'
+export { default as HPView } from './HPView'
+export { default as HPLoader } from './HPLoader'
